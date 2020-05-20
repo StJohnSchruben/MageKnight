4585496a37fe7e84895d31ef94c25bff17e11214
John Schruben <johnschruben@gmail.com>
2020-05-19 19:49:04 -0500

    dragging updated

--- MKView/Views/BattleCanvas.cs
+++ MKView/Views/BattleCanvas.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace MKView.Views
 {
-    public class BattleCanvas
+    public class BattleCanvas : Canvas
     {
+        public BattleCanvas()
+        {
+            this.MouseDown += BattleCanvas_MouseDown;
+            this.PreviewMouseDown += BattleCanvas_PreviewMouseDown;
+        }
+
+        private void BattleCanvas_PreviewMouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ;
+        }
+
+        private void BattleCanvas_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            ;
+        }
+
+        //public bool State
+        //{
+        //    get { return (Boolean)this.GetValue(StateProperty); }
+        //    set { this.SetValue(StateProperty, value); }
+        //}
+
+        //public static readonly DependencyProperty StateProperty = DependencyProperty.Register(
+        //  "State", typeof(bool), typeof(BattleCanvas), new PropertyMetadata(false));
     }
 }
--- MKView/Views/Dial.xaml.cs
+++ MKView/Views/Dial.xaml.cs
@@ -21,15 +21,59 @@ namespace MKView.Views
     /// </summary>
     public partial class Dial : UserControl
     {
+        protected bool isDragging;
+        private Point clickPosition;
+
         public Dial()
         {
             InitializeComponent();
             this.IsVisibleChanged += Dial_IsVisibleChanged;
+            this.MouseLeftButtonDown += new MouseButtonEventHandler(Control_MouseLeftButtonDown);
+            this.MouseLeftButtonUp += new MouseButtonEventHandler(Control_MouseLeftButtonUp);
+            this.MouseMove += new MouseEventHandler(Control_MouseMove);
+
         }
 
         private void Dial_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
             var dial = this.DataContext as IDial;
+        }
+        private void Control_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            var draggableControl = sender as UserControl;
+            clickPosition = e.GetPosition(this);
+            draggableControl.CaptureMouse();
+        }
+
+        private void Control_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            isDragging = false;
+            var draggable = sender as UserControl;
+            draggable.ReleaseMouseCapture();
+        }
+
+        private void Control_MouseMove(object sender, MouseEventArgs e)
+        {
+            var draggableControl = this.FindAncestor<MageKnightBattleView>();
+
+            if (isDragging && draggableControl != null)
+            {
+                Canvas canvas = this.FindAncestor<Canvas>();
+                Point currentCanvasPosition = e.GetPosition(canvas as UIElement);
+                Point currentControlPosition = e.GetPosition(draggableControl as UIElement);
+                var transform = draggableControl.RenderTransform as TranslateTransform;
+                if (transform == null)
+                {
+                    transform = new TranslateTransform();
+                    draggableControl.RenderTransform = transform;
+                }
+
+                double offsetX = draggableControl.ActualWidth - this.ActualWidth;
+                double offsetY = draggableControl.ActualHeight - this.ActualHeight;
+                transform.X = currentCanvasPosition.X - clickPosition.X - (offsetX/2.0);
+                transform.Y = currentCanvasPosition.Y - clickPosition.Y - (offsetY/2.0);
+            }
         }
     }
 }
